67fae8f8daa9c26a4ba163972d2341406b339537
Samyadel123 <113071267+Samyadel123@users.noreply.github.com>
2025-09-26 18:11:51 +0300

    updated clean.py

--- src/clean.py
+++ src/clean.py
@@ -2,16 +2,8 @@ import pandas as pd
 import numpy as np
 from .util import drop_unwanted_features, drop_based_variance, null_elimination
 
-first_drop = [
-    "kepid",
-    "kepoi_name",
-    "kepler_name",
-    "koi_disposition",
-    "koi_tce_delivname",
-]
 
-
-def clean_data(data: pd.DataFrame, target_name: str) -> tuple[pd.DataFrame, pd.Series]:
+def clean_data(data: pd.DataFrame) -> pd.DataFrame:
     """_summary_
 
     Args:
@@ -22,7 +14,8 @@ def clean_data(data: pd.DataFrame, target_name: str) -> tuple[pd.DataFrame, pd.S
         tuple[pd.DataFrame, pd.Series]: cleaned data and target series
     """
     # drop unwanted features
-    data = drop_unwanted_features(features=first_drop, data=data)
+    categorical_cols = data.select_dtypes(include="object").columns
+    data = drop_unwanted_features(features=categorical_cols, data=data)
 
     # drop based on null values
     null_values = data.isna().mean() * 100
@@ -30,13 +23,12 @@ def clean_data(data: pd.DataFrame, target_name: str) -> tuple[pd.DataFrame, pd.S
     to_drop_null = null_elimination(null_values=null_values, threshold=30, names=names)
     data = drop_unwanted_features(features=to_drop_null, data=data)
     # separate target
-    target = data[target_name]
-    data.drop(labels=[target_name], axis=1, inplace=True)
 
     # drop based on variance
     numerical_cols = data.select_dtypes(include=np.number).columns
     to_drop_variance = drop_based_variance(
         threshold=0.1, data=data, numerical_cols=numerical_cols
     )
+
     data = drop_unwanted_features(features=to_drop_variance, data=data)
-    return data, target
+    return data
